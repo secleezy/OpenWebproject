830f32b5af4f8a0d1b5b6a79ccc266e71c1549ac
Hong Beomsun <ghsqjatns@gmail.com>
2023-05-21 23:41:34 +0900

    All product grid add, images copy to src

--- src/pages/all.js
+++ src/pages/all.js
@@ -1,13 +1,44 @@
-import "./UserInterface.css"; //내가만든 css 가져오기
-import { Link } from 'react-router-dom';
+import React from 'react';
+import "./UserInterface.css"; // Import your CSS
 
 export default function All() {
-  return (
-    <div className="all">
-      <ul>
-      <Link to="/login"><li>1번상품</li></Link>
-      </ul>
+    const imgRoute = "./images/item/";
 
-      </div>
-  );
+    const products = [
+        {
+            id: 1,
+            name: "product1",
+            category: "bless",
+        },
+        {
+            id: 2,
+            name: "product2",
+            category: "bless",
+        },
+        {
+            id: 3,
+            name: "product3",
+            category: "bless",
+        },
+
+        {
+            id: 4,
+            name: "product4",
+            category: "bless",
+        }
+    ];
+
+    return (
+        <div className="all">
+            <h1>All Products</h1>
+            <div className="product-grid">
+                {products.map((product) => (
+                    <div className="product-item" key={product.id}>
+                        <img src={imgRoute + product.category + "/" + product.id + ".png"} alt={product.name} />
+                        <h3>{product.name}</h3>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
